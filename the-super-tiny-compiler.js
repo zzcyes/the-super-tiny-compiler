f8cb4da9f5b33ba46834a6779117f29e77cfa7c7
zhongzichen <zhongzichenn@gmail.com>
2023-06-24 19:50:17 +0800

    Add base config and file

---
+++ the-super-tiny-compiler.js
@@ -0,0 +1,14 @@
+// *
+//  *                  LISP                      C
+//  *
+//  *   2 + 2          (add 2 2)                 add(2, 2)
+//  *   4 - 2          (subtract 4 2)            subtract(4, 2)
+//  *   2 + (4 - 2)    (add 2 (subtract 4 2))    add(2, subtract(4, 2))
+//  *
+
+
+// NOTE: 词法分析
+// NOTE: 语法分析
+// NOTE: code => AST
+// NOTE: transform AST
+// NOTE: code generate
